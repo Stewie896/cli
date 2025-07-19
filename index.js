6610e13cdff70e21ef87eebbdc6ec04f6dd3c9e1
Stewie896 <abhiralchhetri6@gmail.com>
2025-07-19 13:02:51 +0530

    secCommit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var hello = () => () => () => (() => {
                 () => {
                     () => {
                         () => {
-                           return console.log("Hey");
+                            console.log("Hey");
                         }
                     }
                 }
@@ -15,5 +15,16 @@ var hello = () => () => () => (() => {
     }
 });
 
-val = hello()()()().j();
-console.log(val);
+
+var sayGooBye = ()=>{
+    return{
+        ByeBye: ()=>{
+            console.log("ByeBye")
+        }
+    }
+}
+
+var val = hello()()()().j();console.log(val);
+var bye = sayGooBye().ByeBye();
+console.log(val)
+console.log(bye)
